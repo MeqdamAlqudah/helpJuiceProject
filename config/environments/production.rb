35b11bd26ff1f4935da659d6bace4e4eaaef92c6
Meqdam Qudah <memuq56@gmail.com>
2022-12-05 21:48:13 +0300

    Try to fix deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,10 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.serve_static_files = false
   config.public_file_server.enabled = false
+  config.assets.compile = true
   config.assets.quiet = true
+  RAILS_ENV=production rake assets:precompile
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
