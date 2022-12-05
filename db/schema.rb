6674a4bd39eb2c0b0d2cfb7ac9341c7a1184736e
Meqdam Qudah <memuq56@gmail.com>
2022-12-05 19:54:34 +0300

    try to fix deploy

--- db/schema.rb
+++ db/schema.rb
@@ -20,4 +20,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_05_013053) do
     t.datetime "updated_at", null: false
     t.integer "search_count", default: 1
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
